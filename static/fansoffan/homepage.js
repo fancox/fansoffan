fb8869632c3d7c938640921c9e403a4e061727e9
Fan Chen <fchen@linkedin.com>
2015-11-02 18:29:39 -0800

    Add homepage card

--- static/fansoffan/homepage.js
+++ static/fansoffan/homepage.js
@@ -1,21 +1,61 @@
 var CoverPhoto = React.createClass({
-  render: function() {
+  render: function () {
     return (
-        (
-            <div className="cover-image" >
-              <div className="main-text">Welcome to Fans of Fan</div>
-              <div className="secondary-text">You will be able to rate and comment on companies (and their associated stock tickers)
-                in various categories such as market growth, profitability, etc.  With these ratings, you''ll
-                be able to interact with other users and improve your speculations on your favorite stocks.</div>
-            </div>)
+        <div className="cover-image"/>
     )
   }
 });
 
-React.render(
-<div>
-  <NavBar/>
-  <CoverPhoto/>
-</div>,
-    document.getElementById('container')
-);
+var CoverText = React.createClass({
+  render: function () {
+    return (
+       <div className="cover-text">
+          <div className="main-text">Fan's secret recipes</div>
+          <div className="secondary-text">This is a food blog that collects recipes Fan has tried. Follow these
+            recipes to feed your loved ones into a fat kitty.
+          </div>
+        </div>
+    )
+  }
+});
+
+/**
+ * @prop imageLink
+ * @prop articleTitle
+ * @prop articleLink
+ * @prop preparationTime
+ * @prop cookingTime
+ */
+var HomePageCard = React.createClass({
+  render: function () {
+    return (
+        <div className="row">
+          <div className="col s4 m4">
+            <div className="card medium">
+              <div className="card-image">
+                <img src={this.props.imageLink}/>
+              </div>
+              <div className="card-content">
+                <p>Preparation time: {this.props.preparationTime} </p>
+                <p>Cooking time: {this.props.cookingTime} </p>
+              </div>
+              <div className="card-action">
+                <a href={this.props.articleLink}>{this.props.articleTitle}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+  }
+});
+
+React.render(<div>
+      <NavBar/>
+      <CoverPhoto/>
+      <CoverText/>
+      <HomePageCard imageLink="img/honey-glazed-salmon.jpg"
+                    articleTitle="Honey Glazed Salmon"
+                    articleLink="/"
+                    preparationTime="20min"
+                    cookingTime="20min"/>
+    </div>, document.getElementById('container'));
