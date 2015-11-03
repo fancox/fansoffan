7c931cff383bfc436cad5dd5402e325a4e1c3a23
Fan Chen <fchen@linkedin.com>
2015-11-02 18:49:50 -0800

    Add more cards

--- static/fansoffan/homepage.js
+++ static/fansoffan/homepage.js
@@ -9,7 +9,7 @@ var CoverPhoto = React.createClass({
 var CoverText = React.createClass({
   render: function () {
     return (
-       <div className="cover-text">
+        <div className="cover-text">
           <div className="main-text">Fan's secret recipes</div>
           <div className="secondary-text">This is a food blog that collects recipes Fan has tried. Follow these
             recipes to feed your loved ones into a fat kitty.
@@ -29,19 +29,18 @@ var CoverText = React.createClass({
 var HomePageCard = React.createClass({
   render: function () {
     return (
-        <div className="row">
-          <div className="col s4 m4">
-            <div className="card medium">
-              <div className="card-image">
-                <img src={this.props.imageLink}/>
-              </div>
-              <div className="card-content">
-                <p>Preparation time: {this.props.preparationTime} </p>
-                <p>Cooking time: {this.props.cookingTime} </p>
-              </div>
-              <div className="card-action">
-                <a href={this.props.articleLink}>{this.props.articleTitle}</a>
-              </div>
+        <div className="col s4 m4">
+          <div className="card medium">
+            <div className="card-image">
+              <img src={this.props.imageLink}/>
+            </div>
+            <div className="card-content">
+              <p>Preparation time: {this.props.preparationTime} </p>
+
+              <p>Cooking time: {this.props.cookingTime} </p>
+            </div>
+            <div className="card-action">
+              <a href={this.props.articleLink}>{this.props.articleTitle}</a>
             </div>
           </div>
         </div>
@@ -50,23 +49,36 @@ var HomePageCard = React.createClass({
 });
 
 var Content = React.createClass({
-  render: function() {
+  render: function () {
     return (
         <div className="content">
-          <HomePageCard imageLink="img/honey-glazed-salmon.jpg"
-                        articleTitle="Honey Glazed Salmon"
-                        articleLink="/"
-                        preparationTime="20min"
-                        cookingTime="20min"/>
+          <div className="row">
+
+            <HomePageCard imageLink="img/honey-glazed-salmon.jpg"
+                          articleTitle="Honey Glazed Salmon"
+                          articleLink="/"
+                          preparationTime="20 min"
+                          cookingTime="20 min"/>
+            <HomePageCard imageLink="img/peanut-butter-cookie.jpg"
+                          articleTitle="Peanut Butter Nutella Sea Salt Cookie"
+                          articleLink="/"
+                          preparationTime="30 min"
+                          cookingTime="60 min"/>
+            <HomePageCard imageLink="img/spinach-pasta.jpg"
+                          articleTitle="Spinach Parmesan Pasta"
+                          articleLink="/"
+                          preparationTime="5 min"
+                          cookingTime="10 min"/>
+          </div>
         </div>
     )
   }
 });
 
 React.render(<div>
-      <NavBar/>
-      <CoverPhoto/>
-      <CoverText/>
-      <Content/>
-      <RcFooter/>
-    </div>, document.getElementById('container'));
+  <NavBar/>
+  <CoverPhoto/>
+  <CoverText/>
+  <Content/>
+  <RcFooter/>
+</div>, document.getElementById('container'));
